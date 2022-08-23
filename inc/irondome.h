6f153516faae2ec06c05a7cb22383da2bc309460
juzzrespecter <danisarp@gmail.com>
2022-08-23 20:08:59 +0200

    layout

---
+++ inc/irondome.h
@@ -0,0 +1,6 @@
+#ifndef IRONDOME_H
+# define IRONDOME_H
+# include <stdlib.h>
+# include <stdio.h>
+
+# endif // IRONDOME_H
---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include "irondome.h"
+
+static void usage(void)
+{
+    dprintf(STDERR_FILENO, "usage: ./irondome DIR [FILE_EXT...]");
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+	usage();
+}
